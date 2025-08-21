a32a295a21efdd4a1f4bb2c1a97ac4e805ad88ef
Duong Song <duongsong9a4@gmail.com>
2025-08-22 06:30:28 +0700

    All here

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Flask server is running!'
+    return send_from_directory('src', 'ItWasPossible.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
